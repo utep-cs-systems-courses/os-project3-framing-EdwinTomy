c2aaf8031915d87ed9d79a7ccdb1871dc2cf5fb9
EdwinTomy <etomygeorg@miners.utep.edu>
2022-04-18 22:51:41 -0600

    used threading instead of fork()

--- project/server.py
+++ project/server.py
@@ -4,37 +4,38 @@
 
 import socket, sys, re, os, time
 import archiver
-sys.path.append("../lib")       # for params
-import params
 
+sys.path.append("../lib")  # for params
+import params, threading
 
 switchesVarDefaults = (
-    (('-l', '--listenPort') ,'listenPort', 50001),
-    (('-?', '--usage'), "usage", False), # boolean (set if present)
-    )
+    (('-l', '--listenPort'), 'listenPort', 50001),
+    (('-?', '--usage'), "usage", False),  # boolean (set if present)
+)
 
-
-
-progname = "echoserver"
+progName = "echoServer"
 paramMap = params.parseParams(switchesVarDefaults)
 
 listenPort = paramMap['listenPort']
-listenAddr = ''       # Symbolic name meaning all available interfaces
+listenAddress = ''  # Symbolic name meaning all available interfaces
 
 if paramMap['usage']:
     params.usage()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((listenAddr, listenPort))
-s.listen(1)              # allow only one outstanding request
+s.bind((listenAddress, listenPort))
+s.listen(1)  # allow only one outstanding request
 # s is a factory for connected sockets
 
+
+def request_handler():
+    print('Connected by', address)
+    byte_arr = archiver.files_to_bytes(["francesco.txt", "sasageyo.txt"])
+    conn.send(byte_arr)
+    conn.shutdown(socket.SHUT_WR)
+
+
 while True:
-    conn, addr = s.accept() # wait until incoming connection request (and accept it)
-    if os.fork() == 0:      # child becomes server
-        print('Connected by', addr)
-        byte_arr = archiver.files_to_bytes(["francesco.txt", "sasageyo.txt"])
-        conn.send(byte_arr)
-        conn.shutdown(socket.SHUT_WR)
-
-
+    conn, address = s.accept()  # wait until incoming connection request (and accept it)
+    thread = threading.Thread(target=request_handler, args=())
+    thread.start()
