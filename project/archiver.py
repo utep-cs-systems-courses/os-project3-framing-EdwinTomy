eb00528213fab4f73cfc7cd3669bf499fdb6ae5b
EdwinTomy <etomygeorg@miners.utep.edu>
2022-04-20 17:23:06 -0600

    can encode/decode images

--- project/archiver.py
+++ project/archiver.py
@@ -1,59 +1,88 @@
-def files_to_bytes(file_lst):
+def fixed_string_num(num, num_digits=10):
 
-    # first section stores pointers of to each file
-    int_arr = [len(file_lst)]
-    for i in range(len(file_lst)):
-        int_arr.append(0)
-
-    for j, file_name in enumerate(file_lst):
-
-        # establishing where this file will be stored
-        init_address = len(int_arr)
-        int_arr[j+1] = init_address
-
-        # stores byte representation of file name and its length
-        int_arr.append(0)
-        file_name_len = 0
-        for i in file_name.encode():
-            file_name_len += 1
-            int_arr.append(i)
-        int_arr[init_address] = init_address+file_name_len
-
-        # stores byte representation of file content
-        content_address = len(int_arr)
-        int_arr.append(0)
-        file_content_len = 0
-        for i in open(file_name, "rb").read():
-            file_content_len += 1
-            int_arr.append(i)
-        int_arr[content_address] = file_content_len+content_address
-
-    bytes_arr = bytearray()
-    for i in int_arr:
-        bytes_arr += i.to_bytes(2, 'big')
-    return bytes_arr
+    num = str(num)
+    while len(num) < num_digits:
+        num = '0' + num
+    return num
 
 
-def bytes_to_files(int_arr):
+def archive(file_name_lst):
+    arr = bytearray()
+    lengths = []
 
-    bytes_arr = []
-    for i in range(len(int_arr)//2):
-        bytes_arr.append(int_arr[2*i] * 2**8 + int_arr[2*i+1])
+    for file_name in file_name_lst:
+        encoded_file_name = bytearray(file_name.encode())
+        arr.extend(encoded_file_name)
+        file_name_len = len(encoded_file_name)
 
-    for j in range(bytes_arr[0]):
-        init_address = bytes_arr[j+1]
-        content_address = bytes_arr[init_address] + 1
+        with open(file_name, "rb") as file:
+            content = file.read()
+            arr.extend(bytes(content))
+            file_content_len = len(content)
 
-        # read file name
-        filename = "new_"
-        for i in bytes_arr[init_address + 1: content_address]:
-            filename += chr(i)
+        lengths.insert(0, fixed_string_num(file_name_len))
+        lengths.insert(0, fixed_string_num(file_content_len))
 
-        # read file content
-        f = open(filename, "w")
-        for i in bytes_arr[content_address + 1: bytes_arr[content_address] + 1]:
-            f.write(chr(i))
+    for length in lengths:
+        arr[0:0] = bytes(length, 'utf-8')
+    arr[0:0] = bytes(fixed_string_num(len(file_name_lst)), 'utf-8')
+
+    return arr
 
 
-byte_arr = files_to_bytes(["fiesta_salsa.txt", "francesco.txt", "sasageyo.txt"])
-bytes_to_files(byte_arr)
+def unarchive(bytes_arr, num_digits=10):
+
+    num_files = int(bytes_arr[:num_digits].decode('utf-8'))
+    lengths = []
+
+    for j in range(num_files):
+        lengths.append(int(bytes_arr[((2 * j + 1) * num_digits):((2 * j + 2) * num_digits)].decode('utf-8')))
+        lengths.append(int(bytes_arr[((2 * j + 2) * num_digits):((2 * j + 3) * num_digits)].decode('utf-8')))
+
+    bytes_arr = bytes_arr[(num_files*2+1)*num_digits:]
+    for j in range(num_files):
+        file_name = bytes_arr[:lengths[2*j]].decode()
+        bytes_arr = bytes_arr[lengths[2*j]:]
+
+        with open('new_' + file_name, "wb") as f:
+            f.write(bytes_arr[:lengths[2*j+1]])
+            bytes_arr = bytes_arr[lengths[2*j+1]:]
+
+
+
+# #byte_arr = files_to_bytes(["fiesta_salsa.txt", "francesco.txt", "sasageyo.txt"])
+# #print(byte_arr[:244])
+# #bytes_to_files(byte_arr)
+#
+# #
+# # x = bytes([105])
+# # print(x)
+# # x = bytes([100])
+# # print(x)
+# # x = bytes([107])
+# # print(x)
+# # x = bytes([112])
+# # print(x)
+# # x = bytes([132])
+# # print(x)
+# # #########
+# # x1 = bytearray(x)
+# # print(x1)
+# # x1 = bytearray(b'idkp\x84vkp\xc8x')
+# # print(x1)
+# #
+# # print(x1[0:3])
+# # print(len(x1))
+#
+# byte_arr = archive(["fiesta_salsa.txt", "francesco.txt", "sasageyo.txt", "image.png"])
+# unarchive(byte_arr)
+# # arr = bytearray()
+# # arr.extend(bytes([127]))
+# # print(arr)  .decode('cp437')
+# # print(arr.decode())
+# # for i in range(20):
+# #     print(str(i))
+# #     print(bytearray(bytes(str(i), 'utf-8')))
+# #
+# #
+# #
